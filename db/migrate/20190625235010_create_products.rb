dc1d462852a09d35b9e8c38a6169f7a80697a55f
Adam Keune <akeune23@gmail.com>
2019-06-25 19:03:40 -0500

    Add seeds to database

--- db/migrate/20190625235010_create_products.rb
+++ db/migrate/20190625235010_create_products.rb
@@ -1,9 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
-      t.string :name
+      t.string :title
+      t.string :author
       t.integer :price
-      t.string :image-url
+      t.string :image_url
       t.string :description
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create({ title: "The Hobbit", author: "J.R.R. Tolkien", price: 15, image_url: "http://2.bp.blogspot.com/-CaKmzMXmD4I/UJ0Y3_HernI/AAAAAAAAN0E/Y3M_HKLCXYA/s1600/hobbit_book_cover_02.jpg", description: "A tale of adventure for a half-sized hero" })
+Product.create({ title: "The Cider House Rules", author: "John Irving", price: 18, image_url: "https://images.bwbcovers.com/034/The-Cider-House-Rules-9780345387653.jpg", description: "Orphan Homer Wells finds goes to live at an apple orchard" })
+Product.create({ title: "The New Jim Crow", author: "Michelle Alexander", price: 22, image_url: "http://en.wikipedia.org/wiki/Special:FilePath/The_New_Jim_Crow_cover.jpg", description: "America's criminal justice system creates a permanent underclass of minorities" })
+Product.create({ title: "American Nations: A History of the Eleven Rival Regional Cultures of North America", author: "Colin Woodard", price: 12, image_url: "http://4.bp.blogspot.com/-nwZf0jkuZls/T_laM-p-8TI/AAAAAAAA5Ms/hW2OAl7-N9U/s1600/American+Nations+cover.JPG", description: "'American culture' is more complex than you think" })
