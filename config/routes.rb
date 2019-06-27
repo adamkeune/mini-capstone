0a120914f0f0c437097388a91b7dd7b255c748ba
Adam Keune <akeune23@gmail.com>
2019-06-26 19:05:41 -0500

    Refactor GET requests to use either query or segment params

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,13 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/all_products" => "products#display_all"
-    get "/hobbit" => "products#display_hobbit"
-    get "/cider" => "products#display_cider"
-    get "/jim" => "products#display_jim"
-    get "/nations" => "products#display_nations"
-    get "/first" => "products#display_first"
+    # get "/hobbit" => "products#display_hobbit"
+    # get "/cider" => "products#display_cider"
+    # get "/jim" => "products#display_jim"
+    # get "/nations" => "products#display_nations"
+    # get "/first" => "products#display_first"
+    get "/one_product" => "products#display_product_query"
+    get "/one_product/:id" => "products#display_product_query"
   end
 end
 2
