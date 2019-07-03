dbc814350fbdf36c520601613f0179a926993433
Adam Keune <akeune23@gmail.com>
2019-07-02 19:23:26 -0500

    Add search functionality to index action

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,11 +1,28 @@
 class Api::ProductsController < ApplicationController
   def index
     @products = Product.all
+
+    if params[:search]
+      @products = @products.where("title ILIKE ? OR author ILIKE ? OR description ILIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%")
+      @products = @products.order(id: :asc)
+    elsif params[:discount]
+      @products = @products.where("price <= ?", 10)
+      @products = @products.order(price: :asc)
+    elsif params[:sort] == "price"
+      if params[:sort_order] == "desc"
+        @products = @products.order(price: :desc)
+      else
+        @products = @products.order(price: :asc)
+      end
+    else
+      @products = @products.order(id: :asc)
+    end
+
     render "index.json.jb"
   end
 
   def create
-    @product = Product.create({
+    @product = Product.new({
       id: params["id"],
       title: params["title"],
       author: params["author"],
@@ -14,7 +31,12 @@ class Api::ProductsController < ApplicationController
       description: params["description"],
       price: params["price"],
     })
-    render "show.json.jb"
+
+    if @product.save
+      render "show.json.jb"
+    else
+      render json: { errors: @product.errors.full_messages }, status: 422
+    end
   end
 
   def show
@@ -29,8 +51,12 @@ class Api::ProductsController < ApplicationController
     @product.image_url = params["image_url"] || @product.image_url
     @product.description = params["description"] || @product.description
     @product.price = params["price"] || @product.price
-    @product.save
-    render "show.json.jb"
+
+    if @product.save
+      render "show.json.jb"
+    else
+      render json: { errors: @product.errors.full_messages }, status: 422
+    end
   end
 
   def destroy
