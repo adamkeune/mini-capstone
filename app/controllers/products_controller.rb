325d24c43b4f4cb425cc546f985dd3e0b8d807ea
Adam Keune <akeune23@gmail.com>
2019-07-16 19:51:07 -0500

    Add edit, update, and destroy actions and views for products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,7 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @suppliers = Supplier.all
     @product = Product.new({
       title: params["title"],
       author: params["author"],
@@ -19,20 +20,58 @@ class ProductsController < ApplicationController
       supplier_id: params["supplier_id"],
     })
 
-    @product.save
-
-    @image = Image.new({
-      url: params["url"],
-      product_id: @product.id,
-    })
-
-    @image.save
-
-    redirect_to "/products/#{@product.id}"
+    if @product.save
+      @image = Image.new({
+        url: params["url"],
+        product_id: @product.id,
+      })
+      @image.save
+      redirect_to "/products/#{@product.id}"
+    else
+      @suppliers = Supplier.all
+      render "new.html.erb"
+    end
   end
 
   def show
     @product = Product.find_by(id: params["id"])
     render "show.html.erb"
+  end
+
+  def edit
+    @suppliers = Supplier.all
+    @image = Image.find_by(product_id: params["id"])
+    @product = Product.find_by(id: params["id"])
+    render "edit.html.erb"
+  end
+
+  def update
+    @suppliers = Supplier.all
+    @product = Product.find_by(id: params["id"])
+    @image = Image.find_by(product_id: params["id"])
+
+    @product.title = params["title"]
+    @product.author = params["author"]
+    @product.description = params["description"]
+    @product.isbn = params["isbn"]
+    @product.price = params["price"]
+    @product.supplier_id = params["supplier_id"]
+    @image.url = params["url"]
+
+    if @product.save
+      @image.save
+      redirect_to "/products/#{@product.id}"
+    else
+      @suppliers = Supplier.all
+      @image = Image.find_by(product_id: params["id"])
+      @product = Product.find_by(id: params["id"])
+      render "edit.html.erb"
+    end
+  end
+
+  def destroy
+    @product = Product.find_by(id: params["id"])
+    @product.destroy
+    redirect_to "/products"
   end
 end
